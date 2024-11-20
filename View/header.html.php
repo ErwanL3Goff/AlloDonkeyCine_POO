5b708f9c53696043e8ce96bd114a9d65a5dc0f51
Erwan <elegoff296@gmail.com>
2024-11-20 16:48:10 +0100

    dossier images

--- View/header.html.php
+++ View/header.html.php
@@ -1,6 +1,29 @@
-<html>
-    <head>
-        <title>Bibliothèque : <?php echo $title; ?></title>
-    </head>
-    <body>
-    <h1><?php echo $title; ?> Controller : <?php echo $action; ?></h1>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Bibliothèque : <?php echo $title; ?></title>
+    <link rel="stylesheet" href="./css/style.css">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/5.1.3/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <div class="logo-container">
+            <a href="index.php">
+                <img src="images/Logo.png" alt="Logo du site" class="logo">
+            </a>
+        </div>
+        <h1><?php echo $title; ?> Controller : <?php echo $action; ?></h1>
+        <style>
+            .Bleach{
+                color: white;
+            }
+        </style>
+        <h2 class="Bleach">AlloDokeyCine</h2>
+        <nav>
+            <a href="index.php">Accueil</a>
+            <a href="reservation.php">Mes Réservations</a>
+            <a href="account.php">Mon Compte</a>
+            <a href="login.php">Connexion</a>
+        </nav>
+    </header>
