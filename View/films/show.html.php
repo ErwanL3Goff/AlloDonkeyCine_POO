798505c977fad53a3f0de4512bbc8141593feff3
Steven SU <stevensu78570@gmail.com>
2024-11-21 10:56:36 +0100

    gitignore file

--- View/films/show.html.php
+++ View/films/show.html.php
@@ -1,2 +1,7 @@
-<?php
-var_dump($film);
+<div class='film-item'>
+    <p>
+    <h3>Titre: <?= $film['titre'] ?></h3>
+    </p>
+    <img src="https://img.freepik.com/vecteurs-libre/vecteur-degrade-logo-colore-oiseau_343694-1365.jpg" />
+    <p>Description: <?= $film['description'] ?></p>
+</div>
