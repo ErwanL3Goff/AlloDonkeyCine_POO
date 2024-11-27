19ad39ee602c1b4f2349d4359dcf68ee36075414
Erwan <elegoff296@gmail.com>
2024-11-27 11:23:20 +0100

    Pablo

--- View/films/show.html.php
+++ View/films/show.html.php
@@ -1,10 +1,12 @@
-<div class="film-container2">
-    <p>
-    <h3>Titre: <?= $film['titre'] ?></h3>
-    </p>
-    <img src="../../view/images/<?= $film['picture'] ?> " class='film-image2' />
-    <p>Description: <?= $film['description'] ?></p>
-    <form action="reservation.php" method="post">
-        <button type="submit" name="idFilmReservation" value="<?= $film['idfilm'] ?>" class="reservation-button">Réserver</button>
-    </form>
+<div class="Pablo">
+    <div class="film-container2">
+        <p>
+        <h3>Titre: <?= $film['titre'] ?></h3>
+        </p>
+        <img src="../../view/images/<?= $film['picture'] ?> " class='film-image2' />
+        <p>Description: <?= $film['description'] ?></p>
+        <form action="reservation.php" method="post">
+            <button type="submit" name="idFilmReservation" value="<?= $film['idfilm'] ?>" class="reservation-button">Réserver</button>
+        </form>
+    </div>
 </div>
